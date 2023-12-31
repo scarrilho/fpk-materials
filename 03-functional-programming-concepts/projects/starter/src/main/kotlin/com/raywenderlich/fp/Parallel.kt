7349fb6121acb838f8707120885a4b8d52b04af4
TE-SergioCarrilho <sergio.carrilho@sony.com>
2024-01-01 01:10:48 +0900

    Chapter3, add Referential transparent/opaque code

--- 03-functional-programming-concepts/projects/starter/src/main/kotlin/com/raywenderlich/fp/Parallel.kt
+++ 03-functional-programming-concepts/projects/starter/src/main/kotlin/com/raywenderlich/fp/Parallel.kt
@@ -28,3 +28,20 @@
  */
 
 package com.raywenderlich.fp
+
+import kotlin.math.sqrt
+
+fun square(x: Double): Double = x * x
+
+fun root(x: Double): Double = sqrt(x)
+
+fun distance(
+    x0: Double,
+    x1: Double,
+    y0: Double,
+    y1: Double
+): Double {
+    val s1 = square(x1 - x0)
+    val s2 = square(y1 - y0)
+    return root(s1 + s2)
+}
--- 03-functional-programming-concepts/projects/starter/src/main/kotlin/com/raywenderlich/fp/ReferentialTransparent.kt
+++ 03-functional-programming-concepts/projects/starter/src/main/kotlin/com/raywenderlich/fp/ReferentialTransparent.kt
@@ -29,3 +29,26 @@
 
 package com.raywenderlich.fp
 
+import com.raywenderlich.fp.util.assertOrThrow
+import kotlin.math.exp
+
+fun main() {
+    // Referentially transparent
+    val expr1 = { 3 * 10 }
+    val (a1, a2) = expr1() to expr1()
+    val expr1Eval = expr1()
+    val (a1Eval, a2Eval) = expr1Eval to expr1Eval
+    assertOrThrow("expr1 is not RT") {
+        a1 == a1Eval && a2 == a2Eval
+    }
+
+    // Referentially opaque
+    var count = 1
+    val expr2 = { 3 * 10 * ++count }
+    val (b1, b2) = expr2() to expr2()
+    val expr2Eval = expr2()
+    val (b1Eval , b2Eval) = expr2Eval to expr2Eval
+    assertOrThrow("expr2 is not RT") {
+        b1 == b1Eval && b2 == b2Eval
+    }
+}
