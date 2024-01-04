44a017b50df0af6cde5c4b75fd3d05dc0102c2e7
TE-SergioCarrilho <sergio.carrilho@sony.com>
2024-01-04 11:45:43 +0900

    Chp5. ReturnFunction

--- 05-higher-order-functions/projects/starter/src/main/kotlin/com/raywenderlich/fp/ReturnFunction.kt
+++ 05-higher-order-functions/projects/starter/src/main/kotlin/com/raywenderlich/fp/ReturnFunction.kt
@@ -28,3 +28,18 @@
  */
 
 package com.raywenderlich.fp
+
+fun countFrom(start: Int): () -> Int {
+    var count = start
+    return {
+        count++
+    }
+}
+
+fun main() {
+    val countFromFive = countFrom(5)
+    println(countFromFive())
+    println(countFromFive())
+    println(countFromFive())
+    println(countFromFive())
+}
