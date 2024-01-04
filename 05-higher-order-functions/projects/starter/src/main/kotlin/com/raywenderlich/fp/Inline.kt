11459dd567eabee3b5e1639b25ff475c842c098f
TE-SergioCarrilho <sergio.carrilho@sony.com>
2024-01-04 23:01:36 +0900

    Chp5. Inline

--- 05-higher-order-functions/projects/starter/src/main/kotlin/com/raywenderlich/fp/Inline.kt
+++ 05-higher-order-functions/projects/starter/src/main/kotlin/com/raywenderlich/fp/Inline.kt
@@ -28,3 +28,26 @@
  */
 
 package com.raywenderlich.fp
+
+fun executor(fn: () -> Unit) {
+    fn()
+}
+
+inline fun executorExecutor(crossinline fn: () -> Unit) {
+    executor {
+        fn()
+    }
+}
+
+fun main() {
+    executor {
+        var count = 0
+        while (true) {
+            count++
+            if (count > 5) {
+                return@executor
+            }
+        }
+    }
+    println("The End!")
+}
