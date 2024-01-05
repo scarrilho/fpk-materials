60a0a46815d44d6fc0c132ab659c1d0871250531
TE-SergioCarrilho <sergio.carrilho@sony.com>
2024-01-05 13:06:49 +0900

    Chp6. Safety code

--- 06-immutability-and-recursion/projects/starter/src/main/kotlin/com/raywenderlich/fp/Safety.kt
+++ 06-immutability-and-recursion/projects/starter/src/main/kotlin/com/raywenderlich/fp/Safety.kt
@@ -29,3 +29,53 @@
  */
 
 package com.raywenderlich.fp
+
+import kotlin.concurrent.thread
+
+data class MutableCounter(
+    var count: Int = 0
+)
+
+val counter = MutableCounter()
+
+val task = {
+    randomDelay()
+    counter.count++
+    randomDelay()
+    if (counter.count == 2) {
+        println("Completed")
+    }
+}
+
+data class Counter(
+    val count: Int = 0
+)
+
+fun incAndCheck(counter: Counter): Counter {
+    randomDelay()
+    val newCounter = Counter(counter.count + 1)
+    randomDelay()
+    if (newCounter.count == 2) {
+        println("Completed")
+    }
+    return newCounter
+}
+
+fun main() {
+    val counter = Counter()
+    lateinit var counter1: Counter
+    val th1 = thread {
+        counter1 = incAndCheck(counter)
+    }
+    th1.join()
+    thread {
+        incAndCheck(counter1)
+    }
+
+}
+
+/*
+fun main() {
+    thread(block = task)
+    thread(block = task)
+}*/
