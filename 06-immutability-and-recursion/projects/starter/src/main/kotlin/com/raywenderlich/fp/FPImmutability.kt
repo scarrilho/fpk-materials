7c0e453a9a9943a70987083d1e3b5c23c1bef7ae
TE-SergioCarrilho <sergio.carrilho@sony.com>
2024-01-05 14:31:19 +0900

    Chp6. Immutability and Recursion

--- 06-immutability-and-recursion/projects/starter/src/main/kotlin/com/raywenderlich/fp/FPImmutability.kt
+++ 06-immutability-and-recursion/projects/starter/src/main/kotlin/com/raywenderlich/fp/FPImmutability.kt
@@ -29,3 +29,23 @@
  */
 
 package com.raywenderlich.fp
+
+/*
+fun main() {
+    var total = 0
+    val list = listOf(1, 5, 10, 12, 34, 55, 80, 23, 35, 12, 80)
+    for (i in 0 until list.size) {
+        if (list[i] % 5 == 0) {
+            total += list[i]
+        }
+    }
+    println("Total: $total")
+}*/
+
+fun main() {
+    val multipleOf5 = { value: Int -> value % 5 == 0}
+    val total = listOf(1, 5, 10, 12, 34, 55, 80, 23, 35, 12, 80)
+        .filter(multipleOf5)
+        .sum()
+    println(total)
+}
--- 06-immutability-and-recursion/projects/starter/src/main/kotlin/com/raywenderlich/fp/Recursion.kt
+++ 06-immutability-and-recursion/projects/starter/src/main/kotlin/com/raywenderlich/fp/Recursion.kt
@@ -29,3 +29,17 @@
  */
 
 package com.raywenderlich.fp
+
+fun recAddMulti5(list: List<Int>): Int {
+    fun loop(i: Int, sum: Int): Int = when {
+        i == list.size -> sum
+        list[i] % 5 == 0 -> loop(i + 1, sum + list[i])
+        else -> loop(i + 1, sum)
+    }
+    return loop(0, 0)
+}
+
+fun main() {
+    val list = listOf(1, 5, 10, 12, 34, 55, 80, 23, 35, 12, 80)
+    println(recAddMulti5(list))
+}
