1e830fa8cda8edeecefd1b3e40700353d2152c7b
TE-SergioCarrilho <sergio.carrilho@sony.com>
2024-01-05 15:26:01 +0900

    Chp6. TailRec

--- 06-immutability-and-recursion/projects/starter/src/main/kotlin/com/raywenderlich/fp/TailRec.kt
+++ 06-immutability-and-recursion/projects/starter/src/main/kotlin/com/raywenderlich/fp/TailRec.kt
@@ -29,3 +29,27 @@
  */
 
 package com.raywenderlich.fp
+
+fun imperativeFactorial(n: Int): Int {
+    var result = 1
+    for (value in 2..n) {
+        result *= value
+    }
+    return result
+}
+
+fun recursiveFactorial(n: Int): Int = when (n) {
+    1 -> 1
+    else -> n * recursiveFactorial(n-1)
+}
+
+tailrec fun tailRecFactorial(n: Int, fact: Int = 1): Int = when (n) {
+    1 -> fact
+    else -> tailRecFactorial(n - 1, n * fact)
+}
+
+fun main() {
+    println(imperativeFactorial(10))
+    println(recursiveFactorial(10))
+    println(tailRecFactorial(10))
+}
