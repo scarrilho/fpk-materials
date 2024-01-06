cc55ba4dad03d34019bd3990df4ec3156645bd65
TE-SergioCarrilho <sergio.carrilho@sony.com>
2024-01-06 23:59:13 +0900

    Ch7. Challenge 3 with operator

--- 07-functional-data-structures/projects/starter/src/main/kotlin/com/raywenderlich/fp/exercises/challenge3.kt
+++ 07-functional-data-structures/projects/starter/src/main/kotlin/com/raywenderlich/fp/exercises/challenge3.kt
@@ -2,15 +2,14 @@ package com.raywenderlich.fp.exercises
 
 import com.raywenderlich.fp.FList
 import com.raywenderlich.fp.head
-import com.raywenderlich.fp.match
 
-fun <T> FList<T>.get(index: Int): T =
+operator fun <T> FList<T>.get(index: Int): T =
     skip(index).head() ?: throw ArrayIndexOutOfBoundsException()
 
 fun main() {
     val list = FList.of(1, 2, 3)
-    println(list.get(0))
-    println(list.get(1))
-    println(list.get(2))
-    println(list.get(3))
+    println(list[0])
+    println(list[1])
+    println(list[2])
+    println(list[3])
 }
