2187dff4a5ad400aa96cf88b6af4130b7b964e04
TE-SergioCarrilho <sergio.carrilho@sony.com>
2024-01-12 18:27:49 +0900

    Chp8. Exercise2 extended

--- 08-composition/projects/starter/src/main/kotlin/com/raywenderlich/fp/exercises/exercise2.kt
+++ 08-composition/projects/starter/src/main/kotlin/com/raywenderlich/fp/exercises/exercise2.kt
@@ -1,5 +1,8 @@
 package com.raywenderlich.fp.exercises
 
+import com.raywenderlich.fp.curry
+import java.lang.Thread.sleep
+
 fun <A, B, C> ((A, B) -> C).flip(): (B, A) -> C =
     { b: B, a: A ->
         this(a, b)
@@ -7,8 +10,25 @@ fun <A, B, C> ((A, B) -> C).flip(): (B, A) -> C =
 
 fun append(a: String, b: String): String = "$a $b"
 
+fun runDelayed(fn: () -> Unit, delay: Long) {
+    sleep(delay)
+    fn()
+}
+
 fun main() {
     val flippedAppend = ::append.flip()
     println(append("First", "Second"))
     println(flippedAppend("First", "Second"))
+
+    runDelayed({
+        println("Delayed")
+    }, 1000)
+
+    val runDelayed1Second = ::runDelayed.flip()
+        .curry()
+        .invoke(1000L)
+
+    runDelayed1Second {
+        println("Delayed")
+    }
 }
