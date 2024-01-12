4923d49d7ab56cfbeb2a044451f0e56c32b2b23b
TE-SergioCarrilho <sergio.carrilho@sony.com>
2024-01-13 01:43:22 +0900

    Chp8. Add code for GeneralComposition

--- 08-composition/projects/starter/src/main/kotlin/com/raywenderlich/fp/GeneralComposition.kt
+++ 08-composition/projects/starter/src/main/kotlin/com/raywenderlich/fp/GeneralComposition.kt
@@ -29,3 +29,38 @@
  */
 
 package com.raywenderlich.fp
+
+typealias Opt<A, B> = (A) -> B?
+
+infix fun <A, B, C> Opt<A, B>.compose(
+    g: Opt<B, C>
+): Opt<A, C> = { a: A ->
+//   this(a)?.let { g(it) }
+   val b = this(a)
+    if (b != null) {
+        g(b)
+    } else {
+        null
+    }
+}
+
+data class User(val id: Int, val username: String)
+
+fun main() {
+    val strToInt = { str: String ->
+        try {
+            str.toInt()
+        } catch (nfe: NumberFormatException) {
+            null
+        }
+    }
+
+    val findUser = { id: Int ->
+        if (id == 3) User(3, "Max") else null
+    }
+    val strToUser = strToInt compose findUser
+    strToUser("a") pipe ::println
+    strToUser("2") pipe ::println
+    strToUser("3") pipe ::println
+}
+
