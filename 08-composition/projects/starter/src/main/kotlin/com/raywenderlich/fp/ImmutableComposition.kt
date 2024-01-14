dc5af1bfc9c552b4d3b640a5757cce9ef8dbf3bb
TE-SergioCarrilho <sergio.carrilho@sony.com>
2024-01-14 22:58:11 +0900

    Chp8. Add code for ImmutableComposition.kt

--- 08-composition/projects/starter/src/main/kotlin/com/raywenderlich/fp/ImmutableComposition.kt
+++ 08-composition/projects/starter/src/main/kotlin/com/raywenderlich/fp/ImmutableComposition.kt
@@ -29,3 +29,25 @@
  */
 
 package com.raywenderlich.fp
+
+data class Counter(
+    val count: Int = 1
+)
+
+fun squareWithImmutableEffect(x: Int): Pair<Int, Updater<Counter>> {
+    val result = x * x
+    return result to { counter -> Counter(counter.count * 10)}
+}
+
+fun doubleWithImmutableEffect(x: Int): Pair<Int, Updater<Counter>> {
+    val result = x * 2
+    return result to { counter -> Counter(counter.count / 2)}
+}
+
+fun main() {
+    val composed = ::squareWithImmutableEffect compose ::doubleWithImmutableEffect compose ::squareWithImmutableEffect
+    val counter = Counter()
+    val (result, comUpdate) = composed(3)
+    result pipe ::println
+    counter pipe comUpdate pipe ::println
+}
