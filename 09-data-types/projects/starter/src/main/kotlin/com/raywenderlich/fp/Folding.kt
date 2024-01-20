8a8b81ba3ef22842c66090a08fece6a831e52bdc
TE-SergioCarrilho <sergio.carrilho@sony.com>
2024-01-21 00:31:07 +0900

    Chp9. Add code for Folding

--- 09-data-types/projects/starter/src/main/kotlin/com/raywenderlich/fp/Folding.kt
+++ 09-data-types/projects/starter/src/main/kotlin/com/raywenderlich/fp/Folding.kt
@@ -29,3 +29,67 @@
  */
 
 package com.raywenderlich.fp
+
+import compose
+
+fun List<Int>.imperativeSum(): Int {
+    var sum = 0
+    for (i in 0 until size) {
+        sum += this[i]
+    }
+    return sum
+}
+
+fun List<Int>.declarativeSum(): Int {
+    tailrec fun helper(pos: Int, acc: Int): Int {
+        if (pos == size) {
+            return acc
+        }
+        return helper(pos + 1, this[pos] + acc)
+    }
+
+    return helper(0, 0)
+}
+
+fun List<Int>.declarativeProduct(): Int {
+    tailrec fun helper(pos: Int, acc: Int): Int {
+        if (pos == size) {
+            return acc
+        }
+        return helper(pos + 1, this[pos] * acc)
+    }
+
+    return helper(0, 1)
+}
+
+fun <T, S> List<T>.declarativeFold(
+    start: S,
+    combineFunc: (S, T) -> S
+): S {
+    tailrec fun helper(pos: Int, acc: S): S {
+        if (pos == size) {
+            return acc
+        }
+        return helper(pos + 1, combineFunc(acc, this[pos]))
+    }
+
+    return helper(0, start)
+}
+
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    list.imperativeSum() pipe ::println
+    List<Int>::imperativeSum compose ::println epip list
+    list.declarativeSum() pipe ::println
+    list.declarativeProduct() pipe ::println
+    list.declarativeFold(0) { acc, item ->
+        acc + item
+    } pipe ::println
+
+    list.declarativeFold(1) { acc, item ->
+        acc * item
+    } pipe ::println
+
+    list.fold(0) { acc, item -> acc + item } pipe ::println
+    list.fold(1) { acc, item -> acc * item } pipe ::println
+}
