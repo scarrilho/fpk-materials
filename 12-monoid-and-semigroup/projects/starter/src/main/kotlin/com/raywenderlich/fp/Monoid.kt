997459473cff1bd56c2c83d8b5729d206e3ea492
TE-SergioCarrilho <sergio.carrilho@sony.com>
2024-01-27 21:38:58 +0900

    Chp12. Add Monoid implementation

--- 12-monoid-and-semigroup/projects/starter/src/main/kotlin/com/raywenderlich/fp/Monoid.kt
+++ 12-monoid-and-semigroup/projects/starter/src/main/kotlin/com/raywenderlich/fp/Monoid.kt
@@ -29,3 +29,18 @@
  */
 
 package com.raywenderlich.fp
+
+interface Monoid<T> {
+    val unit: T
+    val combine: T.(T) -> T
+}
+
+object MonoidIntAdd: Monoid<Int> {
+    override val unit: Int
+        get() = 0
+    override val combine: Int.(Int) -> Int
+        get() = Int::plus
+
+}
+
+
