1ca07e883d11811ad6c982bf835fcb43cbdb3c82
TE-SergioCarrilho <sergio.carrilho@sony.com>
2024-02-03 00:04:35 +0900

    Add code in ValidationSemigroup.kt

--- 14-error-handling-with-fp/projects/starter/fp_kotlin_cap14/src/main/kotlin/com/raywenderlich/fp/validation/ValidationSemigroup.kt
+++ 14-error-handling-with-fp/projects/starter/fp_kotlin_cap14/src/main/kotlin/com/raywenderlich/fp/validation/ValidationSemigroup.kt
@@ -29,3 +29,90 @@
  */
 
 package com.raywenderlich.fp.validation
+
+import com.raywenderlich.fp.applicative.Error
+import com.raywenderlich.fp.applicative.ResultAp
+import com.raywenderlich.fp.applicative.Success
+import com.raywenderlich.fp.applicative.errorMap
+import com.raywenderlich.fp.applicative.successMap
+import com.raywenderlich.fp.lib.curry
+
+interface Semigroup<T> {
+    operator fun plus(rh: T): T
+}
+
+data class ValidationExceptionComposite(
+    private val errors: List<ValidationException>
+) : Exception(), Semigroup<ValidationExceptionComposite> {
+    override fun plus(rh: ValidationExceptionComposite): ValidationExceptionComposite =
+        ValidationExceptionComposite(this.errors + rh.errors)
+
+    override fun getLocalizedMessage(): String {
+        return errors.joinToString { it.localizedMessage }
+    }
+}
+
+fun <E, T, R> ResultAp<E, T>.apsg(
+    fn: ResultAp<E, (T) -> R>
+): ResultAp<E, R> where E: Throwable, E : Semigroup<E> =
+    when (fn) {
+        is Success<(T) -> R> -> successMap(fn.value)
+        is Error<E> -> when(this) {
+            is Success<T> -> Error(fn.error)
+            is Error<E> -> Error(this.error + fn.error)
+        }
+    }
+
+infix fun <E, T, R> ResultAp<E, (T) -> R>.applsg(
+    a: ResultAp<E, T>
+) where E: Throwable, E : Semigroup<E> = a.apsg(this)
+
+fun validateNameSg(
+    name: String
+): ResultAp<ValidationExceptionComposite, String> =
+    if (name.length > 4) {
+        Success(name)
+    } else {
+        Error(ValidationExceptionComposite(
+            listOf(ValidationException("Invalid name"))
+        ))
+    }
+
+fun validateEmailSg(
+    email: String
+): ResultAp<ValidationExceptionComposite, String> =
+    if (email.contains("@")) {
+        Success(email)
+    } else {
+        Error(ValidationExceptionComposite(
+            listOf(ValidationException("Invalid email"))
+        ))
+    }
+
+fun main() {
+    val userBuilder = ::User.curry()
+    val userApplicative = ResultAp.success(userBuilder)
+    val idAp = ResultAp.success(1)
+
+/*    (userApplicative appl
+            idAp appl
+            validateName("") appl
+            validateEmail(""))
+        .errorMap {
+            println("Error: $it"); it
+        }
+        .successMap {
+            println("Success $it")
+        }*/
+
+    (userApplicative applsg
+            idAp applsg
+            validateNameSg("") applsg
+            validateEmailSg(""))
+        .errorMap {
+            println(it.localizedMessage); it
+        }
+        .successMap {
+            println("Success $it")
+        }
+}
